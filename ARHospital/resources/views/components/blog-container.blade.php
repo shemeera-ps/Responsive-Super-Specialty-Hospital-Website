a8f41b6b784dfe3996e30d87d3db637566672aec
shemeera-ps <shemeeraps99@gmail.com>
2024-02-14 17:38:31 +0530

    Uploaded

--- ARHospital/resources/views/components/blog-container.blade.php
+++
@@ -1,8 +0,0 @@
-<section class="w-full my-10 lg:px-20 px-10 py-10 border-b-2 border-orange-700 border-opacity-20">
-    <x-secondary-button class="text-xs w-24">Blog</x-secondary-button>
-    <x-heading class="lg:text-5xl lg:text-left rtl:lg:text-right w-full mb-10">Insights From Our Experts</x-heading>
-    <x-primary-button class="px-10 mt-2" link="{{ route('blogs') }}">View All</x-primary-button>
-    <div class="w-full my-10 grid lg:grid-cols-3 lg:gap-16 gap-y-10">
-       
-    </div>
-</section>
